771d12ab66509aacef5f5522de8a82145fe0151e
fatih <fatih@gmail.com>
2022-07-31 20:34:37 +0300

    Duyuru kısmı ayarlanması gerceklestirelem

--- resources/views/back/duyurular/create.blade.php
+++ resources/views/back/duyurular/create.blade.php
@@ -1,8 +1,16 @@
 @extends('back.layout.master')
 
+@section('css')
+    <link href="{{ asset('back') }}/css/select2.min.css" rel="stylesheet"/>
+@endsection
+
 @section('content')
     <admin-duyuru-create-component
         geriye_don="{{ route('back.duyurular.index') }}"
         duyuru_kategoriler="{{ $duyuru_kategoriler }}"
     ></admin-duyuru-create-component>
+@endsection
+
+@section('js')
+    <script src="{{ asset('back') }}/js/select2.min.js"></script>
 @endsection
