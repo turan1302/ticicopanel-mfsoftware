9538a5a6ab93042348f24f6d3f1f176eddd1c24a
fatih <fatih@gmail.com>
2022-08-02 11:17:56 +0300

    Sosyal medya kısmı ayarlanması gerceklestirildi

--- app/Models/SosyalMedyaModel.php
+++ app/Models/SosyalMedyaModel.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SosyalMedyaModel extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $guarded = [];
+    protected $table = "sosyal_medya";
+    protected $primaryKey = "sm_id";
+
 }
