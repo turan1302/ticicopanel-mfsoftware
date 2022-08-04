a61a14ee034da64534c54f16c28fb47d408b2ed0
fatih <fatih@gmail.com>
2022-08-04 11:12:43 +0300

    Sayfa kısmı ayarlanamsı

---
+++ database/migrations/2022_08_04_081112_create_sayfa_models_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSayfaModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sayfalar', function (Blueprint $table) {
+            $table->id();
+            $table->string('sayfa_resim')->nullable();
+            $table->string('sayfa_baslik')->nullable();
+            $table->string('sayfa_slug')->nullable();
+            $table->text('sayfa_kisa_aciklama')->nullable();
+            $table->text('sayfa_aciklama')->nullable();
+            $table->string('sayfa_title')->nullable();
+            $table->string('sayfa_description')->nullable();
+            $table->string('sayfa_keyword')->nullable();
+            $table->string('sayfa_etiketler')->nullable();
+            $table->string('sayfa_sef_etiketler')->nullable();
+            $table->tinyInteger('sayfa_durum')->default(1)->nullable();
+            $table->string('sayfa_dil_kod')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sayfalar');
+    }
+}
