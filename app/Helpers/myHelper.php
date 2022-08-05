932f59af5a8d4ebe11fef95d8c50bd1710963535
fatih <fatih@gmail.com>
2022-08-05 11:18:01 +0300

    Yetki ayarları kısmı ayarlanmasını gerceklestirelim

---
+++ app/Helpers/myHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Helpers;
+
+class myHelper
+{
+    public static function get_yetkiler(){
+        return [
+            "diller" => "Diller",
+            "servisler" => "Servisler",
+            "duyuru_kategorileri" => "Duyuru Kategorileri",
+            "duyurular" => "Duyurular",
+            "sosyal_medya" => "Sosyal Medya",
+            "sliderlar" => "Sliderlar",
+            "partnerler" => "Partnerler",
+            "ekibimiz" => "Ekibimiz",
+            "sayfalar" => "Sayfalar",
+            "menuler" => "Menüler",
+            "musteri_yorumlari" => "Müşteri Yorumları",
+            "iletisim_mesajlari" => "İletişim Mesajları",
+            "aboneler" => "Aboneler",
+            "sertifikalar" => "Sertifikalar",
+            "duyuru_yorumlar" => "Duyuru Yorumlar",
+            "yetkiler" => "Yetkiler",
+            "kullanicilar" => "Kullanıcılar",
+            "ayarlar" => "Ayarlar",
+        ];
+    }
+}
