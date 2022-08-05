68603412b3dc535bba96da0563807b207984ec63
fatih <fatih@gmail.com>
2022-08-05 11:44:02 +0300

    yetki kısmı ayarlanamsını gerceklestirelim

--- resources/views/back/yetkiler/verilen_yetkiler.blade.php
+++ resources/views/back/yetkiler/verilen_yetkiler.blade.php
@@ -1,8 +1,18 @@
 @extends('back.layout.master')
 
+@section('css')
+    <link href="{{ asset('back') }}/plugins/datatables/datatables.min.css" rel="stylesheet">
+    <link href="{{ asset('back') }}/css/toggle.css" rel="stylesheet">
+@endsection
+
 @section('content')
     <admin-verilen-yetkiler-edit-component
         yetkiler="{{ $yetkiler }}"
         geriye_don="{{ route('back.yetkiler.index') }}"
         yt_id="{{ $item->yt_id }}"></admin-verilen-yetkiler-edit-component>
+@endsection
+
+@section('js')
+    <script src="{{ asset('back') }}/plugins/datatables/datatables.min.js"></script>
+    <script src="{{ asset('back') }}/js/pages/datatables.js"></script>
 @endsection
